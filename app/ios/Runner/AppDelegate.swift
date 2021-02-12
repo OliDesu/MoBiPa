ebc30c57853c99a464aed6a59c9bdda272c770e4
Ali <alielmufti25@gmail.com>
2021-02-12 15:59:14 +0100

    Ajout des dépendances

--- app/ios/Runner/AppDelegate.swift
+++ app/ios/Runner/AppDelegate.swift
@@ -1,6 +1,7 @@
 import UIKit
 import Flutter
 import Firebase
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,6 +9,7 @@ import Firebase
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyAQevl0BZlDKZG3IqLM1Z3DUSQpi3EJWMM")
     GeneratedPluginRegistrant.register(with: self)
     FirebaseApp.configure()
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
