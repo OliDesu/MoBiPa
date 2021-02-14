9f2556cd20219ce30cc57b08627ec37e2cf9b8e0
William Nelson <swilliam.nelson@gmail.com>
2021-02-14 16:24:15 +0100

    [BACKEND] Cartes trello finies pour un utilisateur

--- app/lib/Models/user.dart
+++ app/lib/Models/user.dart
@@ -2,14 +2,37 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:app/Models/utilisateur.dart';
 
-class UserRepo {
+class UserRepo with ChangeNotifier{
     Utilisateur connectedUtilisateur;
 
-    void studentLogin(Student loggedInStudent) {
-        connectedUtilisateur = loggedInStudent;
+    void studentLogin(Utilisateur loggedInUtilisateur) {
+        connectedUtilisateur = loggedInUtilisateur;
+        notifyListeners();
+    }
+
+    void updateUtilisateur(String field, String updateValue) {
+        FirebaseFirestore.instance
+            .collection('utilisateur')
+            .doc(connectedUtilisateur.userId)
+            .update({field: updateValue}).then((result) {
+            switch (field) {
+                case "firstName":
+                    connectedUtilisateur.firstName = updateValue;
+                    break;
+                case "lastName":
+                    connectedUtilisateur.lastName = updateValue;
+                    break;
+                case "tel":
+                    connectedUtilisateur.tel = updateValue;
+                    break;
+            }
+            print("Update $field : $updateValue");
+            notifyListeners();
+        }).catchError((onError) {
+            print("onError");
+        });
     }
 }
-
 class User {
     String userId;
     String firstName;
--- app/lib/Models/utilisateur.dart
+++ app/lib/Models/utilisateur.dart
@@ -4,6 +4,24 @@ class Utilisateur extends User {
     List<String> requests;
 
     Utilisateur(String userId, String firstName, String lastName, String tel, {List<String> requests})
-    : requests = requests ?? new List();
+    : requests = requests ?? new List(),
     super(userId, firstName, lastName, tel);
+
+    factory Utilisateur.fromJson(Map<String, dynamic> json) {
+        return Utilisateur(
+            json['userId'],
+            json['firstName'],
+            json['lastName'],
+            json['tel'],
+            requests: json['requests'] != null ? List<String>.from(json['requests']) : new List(),
+        );
+    }
+
+    Map<String, dynamic> toJson() => {
+        'userId': userId,
+        'firstName': firstName,
+        'lastName': lastName,
+        'tel': tel,
+        'requests': requests,
+    };
 }
