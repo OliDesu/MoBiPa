8b4b3aada6c0976184ccef85767ecba8a9fe1fe1
Ali <alielmufti25@gmail.com>
2021-02-18 17:15:46 +0100

    Ajout d'une deuxieme adresse et possibilité de choisir deux adresses sur deux maps differentes

--- app/lib/homes/home_passenger.dart
+++ app/lib/homes/home_passenger.dart
@@ -13,13 +13,9 @@ class PassengerHome extends StatefulWidget {
 
 class _PassengerHomeState extends State<PassengerHome> {
   static final kInitialPosition = LatLng(-33.8567844, 151.213108);
-  StreamSubscription _locationSubscription;
-  PickResult selectedPlace;
-  Location _locationTracker = Location();
-  Marker marker;
-  Circle circle;
-  GoogleMapController _controller;
 
+  PickResult selectedPlaceStart;
+  PickResult selectedPlaceEnd;
   static final CameraPosition initialLocation = CameraPosition(
     target: LatLng(37.42796133580664, -122.085749655962),
     zoom: 14.4746,
@@ -29,66 +25,6 @@ class _PassengerHomeState extends State<PassengerHome> {
     ByteData byteData =
         await DefaultAssetBundle.of(context).load("assets/directon_icon.png");
     return byteData.buffer.asUint8List();
-  }
-
-  void getCurrentLocation() async {
-    try {
-      Uint8List imageData = await getMarker();
-      var location = await _locationTracker.getLocation();
-
-      updateMarkerAndCircle(location, imageData);
-
-      if (_locationSubscription != null) {
-        _locationSubscription.cancel();
-      }
-
-      _locationSubscription =
-          _locationTracker.onLocationChanged().listen((newLocalData) {
-        if (_controller != null) {
-          _controller.animateCamera(CameraUpdate.newCameraPosition(
-              new CameraPosition(
-                  bearing: 192.8334901395799,
-                  target: LatLng(newLocalData.latitude, newLocalData.longitude),
-                  tilt: 0,
-                  zoom: 18.00)));
-          updateMarkerAndCircle(newLocalData, imageData);
-        }
-      });
-    } on PlatformException catch (e) {
-      if (e.code == 'PERMISSION_DENIED') {
-        debugPrint("Permission Denied");
-      }
-    }
-  }
-
-  void updateMarkerAndCircle(LocationData newLocalData, Uint8List imageData) {
-    LatLng latlng = LatLng(newLocalData.latitude, newLocalData.longitude);
-    this.setState(() {
-      marker = Marker(
-          markerId: MarkerId("home"),
-          position: latlng,
-          rotation: newLocalData.heading,
-          draggable: false,
-          zIndex: 2,
-          flat: true,
-          anchor: Offset(0.5, 0.5),
-          icon: BitmapDescriptor.fromBytes(imageData));
-      circle = Circle(
-          circleId: CircleId("car"),
-          radius: newLocalData.accuracy,
-          zIndex: 1,
-          strokeColor: Colors.blue,
-          center: latlng,
-          fillColor: Colors.blue.withAlpha(70));
-    });
-  }
-
-  @override
-  void dispose() {
-    if (_locationSubscription != null) {
-      _locationSubscription.cancel();
-    }
-    super.dispose();
   }
 
   void pushPage(BuildContext context, Widget page) {
@@ -140,7 +76,7 @@ class _PassengerHomeState extends State<PassengerHome> {
                       }),
                   new ListTile(
                     leading: new Icon(Icons.map),
-                    title: Text('Choisir une adresse de rendez vous'),
+                    title: Text('Nouveau trajet'),
                     onTap: () {
                       Navigator.push(
                         context,
@@ -154,9 +90,30 @@ class _PassengerHomeState extends State<PassengerHome> {
                               selectInitialPosition: true,
                               usePlaceDetailSearch: true,
                               onPlacePicked: (result) {
-                                selectedPlace = result;
+                                selectedPlaceStart = result;
                                 Navigator.of(context).pop();
                                 setState(() {});
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) {
+                                      return PlacePicker(
+                                        apiKey:
+                                            'AIzaSyADXtEYlr02LSSaESs4-tB2yGh0pdtPu0c',
+                                        initialPosition: _PassengerHomeState
+                                            .kInitialPosition,
+                                        useCurrentLocation: true,
+                                        selectInitialPosition: true,
+                                        usePlaceDetailSearch: true,
+                                        onPlacePicked: (result) {
+                                          selectedPlaceEnd = result;
+                                          Navigator.of(context).pop();
+                                          setState(() {});
+                                        },
+                                      );
+                                    },
+                                  ),
+                                );
                               },
                             );
                           },
@@ -186,9 +143,16 @@ class _PassengerHomeState extends State<PassengerHome> {
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
-              selectedPlace == null
+              selectedPlaceStart == null
                   ? Container()
-                  : Text(selectedPlace.formattedAddress ?? ""),
+                  : Text("Adresse de départ : " +
+                          selectedPlaceStart.formattedAddress ??
+                      ""),
+              selectedPlaceEnd == null
+                  ? Container()
+                  : Text("Adresse d'arrivée : " +
+                          selectedPlaceEnd.formattedAddress ??
+                      "")
             ],
           ),
         ));
