b4259ae566682937d18a5632e34c302bd454ed4a
Ali <alielmufti25@gmail.com>
2021-02-23 09:55:08 +0100

    Ajout de toutes les fonctionalités demandées, le tout marche

--- app/lib/homes/home_driver.dart
+++ app/lib/homes/home_driver.dart
@@ -20,56 +20,91 @@ class UserList extends StatefulWidget {
   }
 }
 
-
-// ignore: must_be_immutable
-class _ListPageStates extends StatelessWidget{
-  @override
-
- var firestoreInstance = FirebaseFirestore.instance;
-
-  List<dynamic> orders = [];
-
-  Future fetchOrders() async {
-    QuerySnapshot qn = await firestoreInstance
-        .collection('requests')
-        .where('status',isEqualTo: 'open')
-        .get();
-    return qn.docs;
-  }
+class DetailPage extends StatefulWidget {
+  final DocumentSnapshot post;
+  DetailPage({this.post});
 
   @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      body :
-     Container(
-    child: FutureBuilder(
-    future: fetchOrders(),
-    // ignore: non_constant_identifier_names
-    builder: (_,snapshot){
-    if(snapshot.connectionState == ConnectionState.waiting){
-    return Center(
-    child: Text("Loading ...")
-    );
-    } else{
-    return ListView.builder(
-    itemCount: snapshot.data.length,
-    itemBuilder: (_, index){
-    return ListTile(
-    title: Text(snapshot.data[index]["start"]),
+  _DetailPageState createState() => _DetailPageState();
 
-    );
-    });
-    }
-    }),
-    ),
-    );
-
-  }
 
 
 
 
 }
+
+class _DetailPageState extends State<DetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.post["start"]),
+      ),
+      body: Container(
+        child: Card(
+          child: ListTile(
+
+            title: Text(widget.post["firstName"] +" "+ widget.post["lastName"]),
+            subtitle: Text(widget.post["date"] +"\n" "Adresse de départ : "+widget.post["start"]+"\n"+"Adresse d'arrivée : "+widget.post["destination"]+"\n" )
+            ,
+
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+class _ListPageStates extends StatelessWidget {
+  @override
+  var firestoreInstance = FirebaseFirestore.instance;
+
+
+
+  Future fetchOrders() async {
+    QuerySnapshot qn = await firestoreInstance
+        .collection('requests')
+        .where('status', isEqualTo: 'open')
+        .get();
+    return qn.docs;
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: AppBar(
+        title: Text("Trajets disponibles"),
+      ),
+      body: Container(
+        child: FutureBuilder(
+            future: fetchOrders(),
+            // ignore: non_constant_identifier_names
+            builder: (_, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Center(child: Text("Loading ..."));
+              } else {
+                return ListView.builder(
+                    itemCount: snapshot.data.length,
+                    itemBuilder: (_, index) {
+                      return ListTile(
+                        title: Text(snapshot.data[index]["start"]),
+                        onTap: (){
+                          Navigator.push(context,MaterialPageRoute(builder:(context) =>DetailPage(post:snapshot.data[index],)));
+                        }
+
+                      );
+                    });
+              }
+            }),
+      ),
+    );
+  }
+
+
+}
+
 class _DriverHomeState extends State<DriverHome> {
   Marker marker;
   Circle circle;
@@ -119,10 +154,11 @@ class _DriverHomeState extends State<DriverHome> {
                       leading: new Icon(Icons.directions_car),
                       title: Text('Trajets disponibles'),
                       onTap: () {
-                      Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => _ListPageStates()),
-                      );
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => _ListPageStates()),
+                        );
                       }),
                   new ListTile(
                       leading: new Icon(Icons.settings),
