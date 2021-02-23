d2eda9ccdbab2a034a601789516ecfa61171304f
Ali <alielmufti25@gmail.com>
2021-02-23 10:20:54 +0100

    fix

--- app/lib/homes/home_driver.dart
+++ app/lib/homes/home_driver.dart
@@ -98,6 +98,7 @@ class _DriverHomeState extends State<DriverHome> {
 
   Widget _buildList(BuildContext context, List<DocumentSnapshot> snapshot){
       return ListView(
+
           padding: const EdgeInsets.only(top: 20.0),
           children: snapshot.map((data) => _buildListItem(context, data)).toList(),
       );
@@ -106,20 +107,22 @@ class _DriverHomeState extends State<DriverHome> {
   Widget _buildListItem(BuildContext context, DocumentSnapshot data) {
       final record = Record.fromSnapshot(data);
 
-      return Padding(
-          key: ValueKey(record.firstName),
-          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-          child: Container(
-              decoration: BoxDecoration(
-                  border: Border.all(color: Colors.grey),
-                  borderRadius: BorderRadius.circular(5.0),
-              ),
-              child: ListTile(
-                  title: Text(record.firstName),
-                  trailing: Text(record.lastName),
-                  onTap: () => print(record),
-              ),
-          ),
+      return Scaffold(
+        body: Padding(
+            key: ValueKey(record.firstName),
+            padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+            child: Container(
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(5.0),
+                ),
+                child: ListTile(
+                    title: Text(record.firstName),
+                    trailing: Text(record.lastName),
+                    onTap: () => print(record),
+                ),
+            ),
+        ),
       );
   }
 }
